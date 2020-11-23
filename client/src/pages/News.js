e559fa970287eb5d3f0e665818cbca74d81f447c
Mark Gire <magire01@gmail.com>
2020-11-23 00:01:32 -0500

    Added Date to each post

--- client/src/pages/News.js
+++ client/src/pages/News.js
@@ -16,19 +16,43 @@ const News = () => {
         .catch(err => console.log(err))
     }, [])
 
+    const newsStyle = {
+        postDiv: {
+            width: "90%"
+        },
+        name: {
+            fontSize: 25,
+            paddingLeft: 10,
+            textAlign: "right",
+            paddingRight: 20,
+            paddingTop: 10
+        },
+        text: {
+            fontSize: 17,
+            paddingLeft: 20,
+            paddingRight: 20
+        },
+        date: {
+            textAlign: "center"
+        }
+    }
+
     return (
         <div>
             <Container>
                 <Grid container>
-                    <Grid item md="12">
+                    <Grid item md="12" xs="12">
                         <Typography variant="h3"> News</Typography>
                     </Grid>
                     
-                    <Grid item md="12">
-                    {showNews.newsArr.map(post => (
-                        <Paper>
-                            <Typography>
-                                <p>{post.nickname}</p>
+                    <Grid item md="12" xs="12">
+                    {showNews.newsArr.reverse().map(post => (
+                        <Paper style={newsStyle.postDiv}>
+                            <Typography style={newsStyle.date}> {post.datePosted} </Typography>
+                            <Typography variant="h6" style={newsStyle.name}>
+                                {post.nickname}
+                            </Typography>
+                            <Typography style={newsStyle.text}>
                                 <p>{post.postText}</p>
                             </Typography>
                         </Paper>
