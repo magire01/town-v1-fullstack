7a0a6dedb186ccc4519c02521028c3b6bc4b4b74
Mark Gire <magire01@gmail.com>
2020-11-23 02:57:40 -0500

    Completed basic all towns page

--- client/src/components/TownSelect.js
+++ client/src/components/TownSelect.js
@@ -9,19 +9,71 @@ import CreateProfile from "../components/CreateProfile";
 
 const TownSelect = (props) => {
 
+    const [showTown, setShowTown] = useState({
+        townInfo: []
+    })
+
+    const [selectTown, setSelectTown] = useState({
+        name: null,
+        createdByUser: null,
+        createdByNick: null,
+        description: null,
+        members: [],
+        selected: false
+    })
+
     useEffect(() => {
         API.getTowns()
-            .then(result => console.log(result.data))
+            .then(result => setShowTown({
+                townInfo: result.data
+            }))
             .catch(err => console.log(err))
     }, [])
 
 
-    
+    if (!selectTown.selected)
     return (
         <div>
             <Grid container>
-                All Towns
+                <Grid item md="12">
+                    {showTown.townInfo.map((town) => (
+                        <Grid md="12">
+                            
+                            <Card onClick={() => setSelectTown({ 
+                                ...selectTown,
+                                name: town.name,
+                                createdByUser: town.createdByUser,
+                                createdByNick: town.createdByNick,
+                                description: town.description,
+                                members: town.members,
+                                selected: true
+                                 })} 
+                        >
+                                <Grid item md="12">
+                                    <Button>
+                                        Name: {town.name}
+                                    </Button>
+                                </Grid>
+                                <Grid item md="12">
+                                    <p>
+                                        Description: {town.description}
+                                    </p>
+                                </Grid>
+                                <Grid item md="12">
+                                    <p>
+                                        Created By: {town.createdByNick}
+                                    </p>
+                                </Grid>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
             </Grid>
+        </div>
+    )
+    return (
+        <div>
+            Town Selected
         </div>
     )
 }
